a9ca3450188f4f986ed099e29b972fd38fca68f4
keyur.m <keyur.m@productivet.com>
2021-11-21 21:44:06 +0530

    next added

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -13,19 +13,19 @@ module.exports.addTask = asyncWrapper(async (req, res) => {
     res.status(201).json({message: 'Task Created Successfully.', task});
 });
 
-module.exports.updateTask = asyncWrapper(async (req, res) => {
+module.exports.updateTask = asyncWrapper(async (req, res, next) => {
     let task = await Task.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
     if(!task) next(createCustomError("no task for that id", 400));
     res.status(200).json({message: 'updateTask called', task});
 })
 
-module.exports.deleteTask = asyncWrapper(async (req, res) => {
+module.exports.deleteTask = asyncWrapper(async (req, res, next) => {
     let task = await Task.findOneAndDelete({ _id: req.params.id })
     if(!task) next(createCustomError("no task for that id", 400));
     res.status(200).json({message: 'deleteTask called', task });
 })
 
-module.exports.getTask = asyncWrapper(async (req, res) => {
+module.exports.getTask = asyncWrapper(async (req, res, next) => {
     let task = await Task.findOne({ _id: req.params.id })
     if(!task) next(createCustomError("no task for that id", 400));
     res.status(200).json({message: 'getTask called', task });
